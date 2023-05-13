64fd61bac0bba993d45e2d8ff98c72e1fd80af7d
Benson Kamau <benkabenson@gmail.com>
2023-05-13 12:42:55 +0000

    task done

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""module on pagination"""
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """that takes two integer arguments page with default 
+           value 1 and page_size with default value 10.    
+        """
+        with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+
+        return self.__dataset
+
+    def index_range(page, page_size):
+        """a function that return a tuple of size
+           two containing a start index and an end index 
+        """
+        start_index = page_size * (page - 1)
+        end_index = page * page_size
+        return (start_index, end_index)
+
