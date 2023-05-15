ef6f7b1599fb65cee84665a879369a4056d75441
Benson Kamau <benkabenson@gmail.com>
2023-05-15 10:42:18 +0000

    task

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -53,16 +53,16 @@ class Server:
         except IndexError:
             return []
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+    def get_hyper(self, page: int = 1, page_size: int = 10):
         """that takes two integer arguments page with default
            value 1 and page_size with default value 10.
         """
-        prev_page = page - 1
-        next_page = page + 1
         value = self.dataset()
         total_pages = math.ceil(len(value) / page_size)
         index = self.index_range(page, page_size)
         data = self.get_page(page, page_size)
+        prev_page = page - 1
+        next_page = page + 1
         k = {'page_size': len(data), 'page': page, 'data': data, 'next_page':
              next_page, 'prev_page': prev_page, 'total_pages': total_pages}
         if prev_page == 0:
