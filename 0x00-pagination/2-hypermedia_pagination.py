96cb40cdac532440a7e5406a067b53ce2f481d85
Benson Kamau <benkabenson@gmail.com>
2023-05-15 11:18:17 +0000

    task

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -6,7 +6,6 @@ Module 1-simple_pagination
 import csv
 import math
 from typing import List, Tuple
-from typing import TypedDict
 
 
 class Server:
@@ -28,30 +27,46 @@ class Server:
 
         return self.__dataset
 
-    def index_range(self, page: int, page_size: int) -> Tuple:
-        """a function that return a tuple of size
-           two containing a start index and an end index
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
         """
-        start_index = page_size * (page - 1)
+        Parameters
+        ---------
+        page: int
+            the start of pagination
+        page_size: int
+            the maximum number of object returned
+
+        Returns
+        -------
+        Tuple
+            tuple holding the beginning and ending page numbers
+        """
         end_index = page * page_size
+        start_index = end_index - page_size
         return (start_index, end_index)
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """that takes two integer arguments page with default
-           value 1 and page_size with default value 10.
         """
-        assert type(page_size) is int and type(page) is int
-        assert type(page_size) is int and page_size > 0
-        assert type(page) is int and page > 0
-        index = self.index_range(page, page_size)
-        value = self.dataset()
-        items = []
-        try:
-            for x in range(index[0], index[1]):
-                items.append(value[x])
-            return items
-        except IndexError:
+        Parameters
+        ---------
+        page: int
+            the start of pagination
+        page_size: int
+            the maximum number of object returned
+
+        Returns
+        -------
+        list
+            the appropriate page of the dataset or the correct list of row
+        """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+        result = self.index_range(page, page_size)
+        data = self.dataset()
+        if len(data) < result[0]:
             return []
+        return data[result[0]: result[1]]
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         Parameters
