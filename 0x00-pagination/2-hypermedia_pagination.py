c2f89ebe719ef0e6fb4090f160f60be64f97c715
Benson Kamau <benkabenson@gmail.com>
2023-05-15 12:27:43 +0000

    task done

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -56,16 +56,16 @@ class Server:
             return []
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        """
-        
+        """that takes two integer arguments page with default
+           value 1 and page_size with default value 10.
         """
         total_pages = math.ceil(len(self.dataset()) / page_size)
         data = self.get_page(page, page_size)
-        page_dict = {"page_size": len(data) if page_size >= len(data)
-                     else page_size,
-                    "page": page,
-                    "data": data,
-                    "next_page": page + 1 if page + 1 <= total_pages else None,
-                    "prev_page": page - 1 if page > 1 else None,
-                    "total_pages": total_pages}
+        page_dict = {"page_size": len(data),
+                     "page": page,
+                     "data": data,
+                     "next_page": page + 1 if page + 1 <= total_pages
+                     else None,
+                     "prev_page": page - 1 if page > 1 else None,
+                     "total_pages": total_pages}
         return page_dict
