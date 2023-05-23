3193ce2eac5f9665ceffb005b81363bde8fb16c1
Benson Kamau <benkabenson@gmail.com>
2023-05-23 08:08:30 +0000

    task done

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    """a function that returns index page"""
+    return render_template('0-index.html.html')
+if __name__ == '__main__':
+    app.run(debug=True)
