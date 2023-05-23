3c7fb88ad885c61cd22fe77447eb8b5b4df9115e
Benson Kamau <benkabenson@gmail.com>
2023-05-23 13:04:59 +0000

    task done

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -7,11 +7,16 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
-@app.route('/')
-def index():
-    """a function that returns index page
+class Config:
+    """Config class that has a LANGUAGES class attribute
+       equal to ["en", "fr"]
     """
-    return render_template('2-index.html')
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
 
 
 @babel.localeselector
@@ -19,3 +24,10 @@ def get_locale():
     """determine the best match with our supported languages
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """a function that returns index page
+    """
+    return render_template('2-index.html')
