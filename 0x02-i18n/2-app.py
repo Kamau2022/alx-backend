21d70380fbf048ca05608b8b67c56d269bb795b4
Benson Kamau <benkabenson@gmail.com>
2023-05-23 12:32:18 +0000

    done

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 """a module on babel
 """
-from flask import request
+from flask import Flask, render_template
 from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
-@babel.localeselector
 
+@app.route('/')
+def index():
+    """a function that returns index page
+    """
+    return render_template('2-index.html')
+
+
+@babel.localeselector
 def get_locale():
     """determine the best match with our supported languages
     """
