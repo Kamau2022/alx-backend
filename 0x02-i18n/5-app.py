37fcd9e9a2c7579d29ccddf0c1d6f0e49727f4ce
Benson Kamau <benkabenson@gmail.com>
2023-05-24 10:47:11 +0000

    task done

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -48,14 +48,9 @@ def before_request() -> None:
 def get_locale() -> str:
     """determine the best match with our supported languages
     """
-    queries = request.query_string.decode('utf-8').split('&')
-    query_table = dict(map(
-        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
-        queries,
-    ))
-    if 'locale' in query_table:
-        if query_table['locale'] in app.config["LANGUAGES"]:
-            return query_table['locale']
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
