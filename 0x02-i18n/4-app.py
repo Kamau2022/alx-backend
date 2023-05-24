65d03b802f15627c0eae21c913dc21d55e0d28bb
Benson Kamau <benkabenson@gmail.com>
2023-05-24 07:44:35 +0000

    to be fixed

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""a module to get locale from request
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+AVAILABLE_LOCALES = ('fr')
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Config class that has a LANGUAGES class attribute
+       equal to ["en", "fr"]
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+@babel.localeselector
+def get_locale(self) -> str:
+    """determine the best match with our supported languages
+    """
+    locale = self.request.get('locale')
+    if locale in AVAILABLE_LOCALES:
+        return 'fr'
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """a function that returns index page
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
