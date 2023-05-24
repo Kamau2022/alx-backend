1bfdc883506cee005719b80d8d013a75f63adf15
Benson Kamau <benkabenson@gmail.com>
2023-05-24 08:32:53 +0000

    fixed

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -5,7 +5,6 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
-app.config.from_object(Config)
 babel = Babel(app)
 
 
@@ -16,6 +15,9 @@ class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
 
 
 @babel.localeselector
